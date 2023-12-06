196b81721defc49ee640cf6b9c767225694da9af
rafika <rafikaa.sid@gmail.com>
2023-12-06 11:51:42 +0100

    ajout fichier argument, graphe

---
+++ src/argument.py
@@ -0,0 +1,10 @@
+class Argument:
+    def __init_(self,nom):
+        self.nom =nom
+    
+
+class Graphe:
+    def __init__(self, arguments, relations):
+        self.arguments=arguments
+        self;relations=relations
+        
